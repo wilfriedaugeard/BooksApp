ff1121903685c09469e0f95b55b3bcf16a4e1e5d
Thimotée <brocard.thimotee@gmail.com>
2020-11-24 17:13:13 +0100

    Ajout dbname dans l'uri

--- src/back/server.js
+++ src/back/server.js
@@ -1,5 +1,5 @@
 const MongoClient = require('mongodb').MongoClient;
-const uri = "mongodb+srv://bookapp:bookapp@bookapps.vng4w.mongodb.net/<dbname>?retryWrites=true&w=majority";
+const uri = "mongodb+srv://bookapp:bookapp@bookapps.vng4w.mongodb.net/bookapps?retryWrites=true&w=majority";
 const client = new MongoClient(uri, { useNewUrlParser: true });
 client.connect(err => {
   const collection = client.db("test").collection("devices");
