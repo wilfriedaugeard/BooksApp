0843038819331e14e00d151f8f85226e4427782d
Thimotée Brocard <brocard.thimotee@gmail.com>
2020-12-10 15:15:48 +0100

    correction typo : la recherche se fait (code 200) mais donne aucun resultat

--- backend/app.js
+++ backend/app.js
@@ -48,7 +48,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/users', usersRouter);
-app.use('/book', booksRouter);
+app.use('/books', booksRouter);
 
 
 // catch 404 and forward to error handler
