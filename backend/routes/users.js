010904565a1ae0d090905dee021f19dbd537999f
Thimotée Brocard <brocard.thimotee@gmail.com>
2020-12-24 22:27:25 +0100

    to pull

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -9,6 +9,7 @@ router.post('/login', userControler.logInUser);
 
 
 router.get('/user', userControler.isConnectedUser, function (req, res, next) {
+    console.log(req.user)
     return res.status(200).json(req.user);
 });
 
