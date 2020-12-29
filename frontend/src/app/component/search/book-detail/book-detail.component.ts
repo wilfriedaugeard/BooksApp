0892f008ac180e4d85b578b9fbd279b0ef233ab1
wilfriedaugeard <wilfried.augeard@etu.u-bordeaux.fr>
2020-12-29 17:54:53 +0100

    Bouton d'ajout aux listes seulement pour user co

--- frontend/src/app/component/search/book-detail/book-detail.component.ts
+++ frontend/src/app/component/search/book-detail/book-detail.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { SearchService } from '../../../service/search/search.service';
+import { UserService } from '../../../service/user/user.service'
 
 @Component({
   selector: 'app-book-detail',
@@ -11,17 +12,27 @@ export class BookDetailComponent implements OnInit {
   @Input() found: boolean = false;
   @Input() length: number = 0;
   currentBook: any;
+  isAuth: boolean;
 
-  constructor(private _searchService: SearchService) { }
+  constructor(private _searchService: SearchService, private _userService: UserService) { }
 
   ngOnInit(): void {
     this.currentBook = this._searchService.getChosenBook();
+    this.isAuth = this._userService.isAuth;
     this._searchService.sub(this);
+    this._userService.sub(this);
   }
 
   setChosenBook(book: any){
     this.currentBook = book;
   }
+
+  setIsAuth(value: boolean){
+    this.isAuth = value;
+  }
+
+
+
 
 
 }
