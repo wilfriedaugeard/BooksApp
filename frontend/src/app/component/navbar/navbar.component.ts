06f5545e7f37af189e77b97d00401d3bb37d1fc2
wilfriedaugeard <wilfried.augeard@etu.u-bordeaux.fr>
2020-12-29 13:21:48 +0100

    Ajout du pattern Observateur sur l'auth/navbar

--- frontend/src/app/component/navbar/navbar.component.ts
+++ frontend/src/app/component/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { UserService } from '../../service/user/user.service';
 
 @Component({
   selector: 'app-navbar',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
 
-  constructor() { }
+  isAuth: boolean = false;
+
+  constructor(private _router: Router, private _userService: UserService) { }
 
   ngOnInit(): void {
+    this._userService.sub(this);
+    console.log('isAuth in nav: '+this.isAuth)
+  }
+
+  logout() {
+    this._userService.logout()
+      .subscribe(
+        data => {
+          console.log(data);
+          this._userService.validateLogout();
+          this._router.navigate(['/homepage']);
+        },
+        error => console.error(error))
+  }
+
+  setIsAuth(value: boolean){
+    this.isAuth = value;
+    this.ngOnInit();
   }
 
 }
