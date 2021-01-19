11da093175d83ac623e06010dbe6a98d4c14f7ca
Thimotée Brocard <brocard.thimotee@gmail.com>
2021-01-19 18:36:45 +0100

    log

--- frontend/src/app/component/search/search.component.ts
+++ frontend/src/app/component/search/search.component.ts
@@ -72,7 +72,7 @@ export class SearchComponent implements OnInit {
         const currency = (data.saleInfo.listPrice !== undefined) ? data.saleInfo.listPrice.currencyCode : '';
         const price = priceValue + ' ' + currency;
         const title = data.volumeInfo.title ? data.volumeInfo.title : 'unknow';
-        console.log(data.recommendationList);
+        // console.log(data.recommendationList);
         const recommendationList = data.recommendationList ? data.recommendationList.map((book: any) => this.formatBook(book)) : [];
         let formattedBook =
         {
